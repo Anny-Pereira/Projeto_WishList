15aae822958a452d94c89cfa5515220d2d3baf3e
EMarcolino <estevan.marc10@gmail.com>
2021-10-27 19:08:55 -0300

    Configurando Cors - Startup

---
+++ Backend/Senai_WishList_WebApi/Senai_WishList_WebApi/Senai_WishList_WebApi/Contexts/WishListContext.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Senai_WishList_WebApi.Domains;
+
+#nullable disable
+
+namespace Senai_WishList_WebApi.Contexts
+{
+    public partial class WishListContext : DbContext
+    {
+        public WishListContext()
+        {
+        }
+
+        public WishListContext(DbContextOptions<WishListContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Desejo> Desejos { get; set; }
+        public virtual DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source = MARC-NOTE; initial catalog= Desafio_WishList; user Id = sa; pwd=marcsql;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            modelBuilder.Entity<Desejo>(entity =>
+            {
+                entity.HasKey(e => e.IdDesejo)
+                    .HasName("PK__Desejos__5DB1341F8F13D784");
+
+                entity.Property(e => e.IdDesejo)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idDesejo");
+
+                entity.Property(e => e.DescDesejo)
+                    .HasColumnType("text")
+                    .HasColumnName("desc_desejo");
+
+                entity.Property(e => e.IdUsuario).HasColumnName("idUsuario");
+
+                entity.HasOne(d => d.IdUsuarioNavigation)
+                    .WithMany(p => p.Desejos)
+                    .HasForeignKey(d => d.IdUsuario)
+                    .HasConstraintName("FK__Desejos__idUsuar__267ABA7A");
+            });
+
+            modelBuilder.Entity<Usuario>(entity =>
+            {
+                entity.HasKey(e => e.IdUsuario)
+                    .HasName("PK__Usuarios__645723A61CC78BD4");
+
+                entity.Property(e => e.IdUsuario)
+                    .ValueGeneratedOnAdd()
+                    .HasColumnName("idUsuario");
+
+                entity.Property(e => e.Email)
+                    .HasColumnType("text")
+                    .HasColumnName("email");
+
+                entity.Property(e => e.Senha)
+                    .HasColumnType("text")
+                    .HasColumnName("senha");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Backend/Senai_WishList_WebApi/Senai_WishList_WebApi/Senai_WishList_WebApi/Repositories/DesejoRepository.cs
@@ -0,0 +1,28 @@
+﻿using Senai_WishList_WebApi.Contexts;
+using Senai_WishList_WebApi.Domains;
+using Senai_WishList_WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_WishList_WebApi.Repositories
+{
+    public class DesejoRepository : IDesejoRepository
+    {
+        WishListContext ctx = new WishListContext();
+
+        public void Cadastrar(Desejo novoDesejo)
+        {
+            ctx.Desejos.Add(novoDesejo);
+
+            ctx.SaveChanges();
+        }
+
+
+        public List<Desejo> ListarTodos()
+        {
+            return ctx.Desejos.ToList();
+        }
+    }
+}
