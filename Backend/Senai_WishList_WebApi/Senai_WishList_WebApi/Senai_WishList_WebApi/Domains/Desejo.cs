6386c155926cf168f10e5d2ef88b2b9b405c2caa
Anny Pereira <isabelle.pereira04@gmail.com>
2021-10-27 08:21:10 -0300

    Api - Listar e Cadastrar Desejos

---
+++ Backend/Senai_WishList_WebApi/Senai_WishList_WebApi/Senai_WishList_WebApi/Domains/Desejo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_WishList_WebApi.Domains
+{
+    public class Desejo
+    {
+
+        public int IdDesejo { get; set; }
+
+        public string descricao { get; set; }
+
+    }
+}
---
+++ Backend/Senai_WishList_WebApi/Senai_WishList_WebApi/Senai_WishList_WebApi/Repositories/DesejoRepository.cs
@@ -0,0 +1,27 @@
+﻿using Senai_WishList_WebApi.Domains;
+using Senai_WishList_WebApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai_WishList_WebApi.Repositories
+{
+    public class DesejoRepository : IDesejoRepository
+    {
+        //Contexto 
+
+        public void Cadastrar(Desejo novoDesejo)
+        {
+            ctx.Desejos.Add(novoDesejo);
+
+            ctx.SaveChanges();
+        }
+
+
+        public List<Desejo> ListarTodos()
+        {
+            return ctx.DesejosToList();
+        }
+    }
+}
